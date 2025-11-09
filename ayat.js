9cb57a4d8d979bd8fbc70b0b8388522792f712fb
amin20033 <137201541+amin20033@users.noreply.github.com>
2025-11-09 17:42:44 +0530

    Add files via upload

--- ayat.js
+++ ayat.js
@@ -1,13 +1,15 @@
 let surahURL = "https://quranapi.pages.dev/api/surah.json";
 let btn = document.querySelector("button");
-let h1 = document.querySelector("h1");
+let h1 = document.querySelector(".h1");
+let bengali = document.querySelector(".bengali");
+let urdu = document.querySelector(".urdu");
 let p1 = document.querySelector(".p1");
 let p2 = document.querySelector(".p2");
 let p3 = document.querySelector(".p3");
 let h2=document.querySelector("h2")
 async function runData() {
   let data = await fetch(surahURL);
-  let randSurahNum = Math.floor(Math.random() * 114);
+  let randSurahNum = (Math.floor(Math.random() * 114))+1;
   let readData = await data.json();
   let totalAyat = Number(readData[randSurahNum]["totalAyah"]);
   let randAyat = Math.floor(Math.random() * totalAyat) + 1;
@@ -20,6 +22,8 @@ async function runData() {
   p2.innerHTML = "Ayat Number: " + readVerse["ayahNo"];
   p3.textContent = readVerse["arabic1"];
   h2.style.display="block"
+  urdu.innerHTML=readVerse["urdu"]
+  bengali.innerHTML=readVerse["bengali"]
   let audio = readVerse["audio"]["1"]["url"];
   document.querySelector("a").setAttribute("href",`${audio}`)
 }
